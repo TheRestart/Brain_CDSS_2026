cabcfaa3e443f8ef9ca4993413ae0e39c485523c
SmileBBDev <dtp03137@gmail.com>
2026-01-30 12:51:13 +0900

    역할별 색상바 크기 조절

--- brain_tumor_front/src/layout/AppHeader.tsx
+++ brain_tumor_front/src/layout/AppHeader.tsx
@@ -73,7 +73,7 @@ export default function AppHeader({ onToggleSidebar }: AppHeaderProps) {
 
     <div
       style={{
-        height: 4,
+        height: 10,
         background: theme.bg,
       }}
     />
